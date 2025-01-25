6c18d401f5523ff8d83450076b8bb2e039f8aa9e
Лида <kolidtka@yandex.ru>
2025-01-25 11:15:11 +0300

    коллизии

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from random import randint
+
 from settings import *
 from player import Player
+from sprites import *
 
 
 class Game:
@@ -13,14 +16,19 @@ class Game:
 
         # groups 
         self.all_sprites = pygame.sprite.Group()
+        self.collision_sprites = pygame.sprite.Group()
 
         # sprites
-        self.player = Player((400, 300), self.all_sprites)
+        self.player = Player((400, 300), self.all_sprites, self.collision_sprites)
+        for i in range(6):
+            x, y = randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)
+            w, h = randint(60, 100), randint(50, 100)
+            CollisionSprite((x, y), (w, h), (self.all_sprites, self.collision_sprites))
 
     def run(self):
         while self.running:
             # dt 
-            dt = self.clock.tick() / 1000
+            dt = self.clock.tick() / 1000  # дататайм
 
             # event loop 
             for event in pygame.event.get():
