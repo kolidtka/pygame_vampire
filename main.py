2a401b4348e03010ae2b8f1be80d2723ed092fdd
Лида <kolidtka@yandex.ru>
2025-01-25 12:49:23 +0300

    реализация камеры 2

--- main.py
+++ main.py
@@ -25,10 +25,9 @@ class Game:
         self.setup()
 
         # sprites
-        self.player = Player((500, 300), self.all_sprites, self.collision_sprites)
-
     def setup(self):
         map = load_pygame(join("data", "maps", "world.tmx"))
+
         for x, y, image in map.get_layer_by_name("Ground").tiles():
             Sprite((x * TILE_SIZE, y * TILE_SIZE), image, self.all_sprites)
 
@@ -37,6 +36,10 @@ class Game:
 
         for obj in map.get_layer_by_name("Collisions"):
             CollisionSprite((obj.x, obj.y), pygame.Surface((obj.width, obj.height)), self.collision_sprites)
+
+        for obj in map.get_layer_by_name("Entities"):
+            if obj.name == "Player":
+                self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
 
     def run(self):
         while self.running:
