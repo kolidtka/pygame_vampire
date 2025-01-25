f48b2faf0f3a301e2ab1c1e6c1a6b5212b73f201
Лида <kolidtka@yandex.ru>
2025-01-25 12:11:29 +0300

    импорт всего из Tiled в игру

--- sprites.py
+++ sprites.py
@@ -3,8 +3,15 @@ import pygame
 from settings import *
 
 
+class Sprite(pygame.sprite.Sprite):
+    def __init__(self, pos, surf, groups):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_frect(topleft=pos)
+
+
 class CollisionSprite(pygame.sprite.Sprite):
     def __init__(self, pos, surf, groups):
         super().__init__(groups)
         self.image = surf
-        self.rect = self.image.get_frect(center=pos)
+        self.rect = self.image.get_frect(topleft=pos)
